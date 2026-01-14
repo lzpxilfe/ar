b2878eede89edc52ba08c2cbbf5c93d183c33b4a
balguljang2 <balguljang2@example.com>
2026-01-14 16:48:23 +0900

    v1.5.69: Complete restoration, deep cleanup, and code review. Optimized cumulative viewshed (bit-flags + circular masking) finalized.

--- tools/viewshed_dialog.py
+++ tools/viewshed_dialog.py
@@ -2,19 +2,7 @@
 
 # ArchToolkit - Archaeology Toolkit for QGIS
 # Copyright (C) 2026 balguljang2
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# License: GPL v3
 """
 Viewshed Analysis Dialog for ArchToolkit
 Visibility analysis for archaeological applications: fortifications, temples, etc.
